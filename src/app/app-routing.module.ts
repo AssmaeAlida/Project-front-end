9f62e776f049d7985e60bb40e262e24df7090610
ilhameaguer <ilhameaguergour@gmail.com>
2023-04-14 12:32:21 +0100

    home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,16 @@ import {
 import {BudgetCreateComponent} from "./view/Budgets/budget-create/budget-create.component";
 import {AppelAchatCreateComponent} from "./view/AppelAchat/appel-achat-create/appel-achat-create.component";
 import {HomeComponent} from "./view/home/home.component";
+import {PageNoteFoundedComponent} from "./view/page-note-founded/page-note-founded.component";
 
 const routes: Routes =[
+  {path: '' , component:HomeComponent},
   { path: 'entite-administratif-create', component:EntiteAdministratifCreateComponent},
   { path: 'expression-besoin-create', component:ExpressionBesoinCreateComponent},
   { path: 'budget-create', component:BudgetCreateComponent},
   { path: 'home', component: HomeComponent},
-  { path: 'appel-achat-create', component:AppelAchatCreateComponent}
+  { path: 'appel-achat-create', component:AppelAchatCreateComponent},
+  {path:'**',component:PageNoteFoundedComponent}
 ]
 
 @NgModule({
